09e2194fb57d9c111a867cd5e4978ea4ca2a8f4c
Nikita5152 <114350496+Nikita5152@users.noreply.github.com>
2023-05-14 23:18:34 +0300

    Add files via upload

---
+++ WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket socket;
+        private List<Socket> clients = new List<Socket>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect("26.16.20.0", 8888);
+            RecieveMessage();
+        }
+
+        private async Task RecieveMessage()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte [1024];
+                await socket.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                List.Items.Add(message);
+
+            }
+        }
+
+        private async Task SendMessage(string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await socket.SendAsync(bytes, SocketFlags.None);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(Tekst.Text);
+        }
+    }
+}
